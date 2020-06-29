9b63bd3fbf7eddc385d8dd1807c181ade542423f
Michael-F-Bryan <michaelfbryan@gmail.com>
2020-06-29 18:34:12 +0800

    Switched to nalgebra for the matrix type and implemented newton's method

--- src/algebra/mod.rs
+++ src/algebra/mod.rs
@@ -2,11 +2,9 @@
 
 mod equations;
 mod expr;
-mod matrix;
 pub mod ops;
 mod parse;
 
 pub use equations::{Equation, SystemOfEquations};
 pub use expr::{BinaryOperation, Expression, Parameter};
-pub use matrix::Matrix;
 pub use parse::{parse, ParseError, TokenKind};
